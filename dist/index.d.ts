f4ffb7eff522aa8d2bf361d13ffad67442ba5449
Piyush Mehta <me@piyushmehta.com>
2026-09-17 20:04:19 -0400

    Include built plugin distribution

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import * as _opencode_ai_plugin_v2_promise from '@opencode-ai/plugin/v2/promise';
+
+declare const _default: _opencode_ai_plugin_v2_promise.Plugin;
+
+export { _default as default };
